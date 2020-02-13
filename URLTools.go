479a2015d4f444a66c1fbf5ac1a55dab26cfb965
jojiiOfficial <jojii@jojii.de>
2020-02-13 12:14:09 +0100

    add ParseURL

--- URLTools.go
+++ URLTools.go
@@ -13,6 +13,13 @@ func (u *URL) JoinPath(spath string) {
 	u.Path = path.Join(u.Path, spath)
 }
 
+//ParseURL parses an URL to a goawesomehelper.URL
+func ParseURL(u string) (*URL, error) {
+	purl, err := url.Parse(u)
+	ue := (URL)(*purl)
+	return &ue, err
+}
+
 //URLJoinPath join path to url
 func URLJoinPath(surl, spath string) (string, error) {
 	u, err := url.Parse(surl)
